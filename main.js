d799a830a6da113762e3954bbef255fed2532481
kontakt <kontakt@mtgkort.dk>
2020-11-27 12:37:26 +0100

    sdf

--- main.js
+++ main.js
@@ -203,7 +203,7 @@
           });
       svg.call(zoom);
       var render = new dagreD3.render();
-      console.log("Dagre Rendered 10")
+      console.log("Dagre Rendered 11")
       
       render(inner, g);
   
